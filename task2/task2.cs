4aab36ba0f7d69eab88630c0fa63a9b2855e1b18
Igor Kamerzan <ink3091245@gmail.com>
2021-02-28 01:09:09 +0300

    Добавьте файлы проекта.

---
+++ task2/task2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace task2
+{
+    class task2
+    {
+        static void Main(string[] args)
+        {
+
+            //просим пользователя ввести порядковый номер месяца
+            Console.WriteLine("Введите порядковый номер месяца");
+            
+            // cоздаем переменную monthnumber для записи введенного значения
+            string monthNumber = Console.ReadLine();
+
+            //создаем переменную типа int, в которую будут преобразованы данные, введенные пользователем
+            int monthNumberConverted;
+                     
+            //преобназуем строковую переменную, введеную пользователм, в целочисленную
+            int.TryParse(monthNumber, out monthNumberConverted);
+
+            // если пользователь ввел значение меньше 1 и больше 12, то повтор ввода значения 
+            while (monthNumberConverted > 12 || monthNumberConverted < 1)
+            
+            {
+                Console.WriteLine("Вы ввели некорректное значение\n" +
+                    "введите значение от 1 до 12");
+                monthNumber = Console.ReadLine();
+                int.TryParse(monthNumber, out monthNumberConverted);
+            }
+
+
+
+             //cоздаем перменную для записи даты произвольного форматирования
+             DateTime userdate = new DateTime(2021, monthNumberConverted, 26);
+
+            //преобразуем формат переменной для вывода только месяца
+             userdate.ToString("MMMM");
+
+            //выводим на консоль месяц
+            Console.WriteLine("Вы ввели " + userdate.ToString("MMMM") + ".");
+           
+            //просим пользователя ввести любую клавишу для завершения программы
+            Console.WriteLine("Нажмите любую клавишу для завершения программы");
+            Console.ReadKey();
+            
+        }
+        
+    }
+}
---
+++ task3/task3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace task3
+{
+    class task3
+    {
+        static void Main(string[] args)
+        {
+                        
+            //просим пользователя число
+            Console.WriteLine("Введите число");
+            
+            //создаем переменную для введенного значения
+            string inputNumber = Console.ReadLine();
+
+            //создаем переменную для преобразования введенного значения в целочисленный тип
+            int outputNumber;
+
+            //создаем булевскую переменную со значением true, если преобразование в целочисленный тип пройдет успешно
+            bool convertionSuccess = int.TryParse(inputNumber, out outputNumber);
+
+            // проверяем, прошло ли преобразование в целочисленный тип
+            if (convertionSuccess)
+            {
+                if (outputNumber % 2 == 0) // проверка доп условаия (остаток от деления числа на 2 = 0
+                {
+                    Console.WriteLine("Вы ввели четное число");
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели нечетное число"); //если есть остаток, то пишем, что число не целое 
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Извольте ввести целое число"); //если конверсия в целое число не удалась, то написать, чтобы ввели целое
+            }
+            
+
+            // Завершение программы по клавише
+            Console.WriteLine("Нажмите любую клавишу");
+            Console.ReadKey();
+
+        }
+    }
+}
